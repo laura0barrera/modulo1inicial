57f6cf0642f34d93eeb2197a242fb672f82a3bcc
laura0barrera <124647475+laura0barrera@users.noreply.github.com>
2023-02-10 18:35:15 -0500

    tarea
    se realizo la tarea de variables, float, double, decimal.

--- Program.cs
+++ Program.cs
@@ -17,6 +17,29 @@
             string nom = Console.ReadLine();
             Console.WriteLine("el nombre del susurio es " + nom);
             Console.ReadKey();
+
+            Console.WriteLine("LAURA GIL");
+
+            Console.WriteLine("INT");
+            int numerosupergrande = int.MaxValue;
+            Console.WriteLine(numerosupergrande);
+
+            //Tipos flotantes 
+            float altura = 180.123456789f; //ocupa 4 bytes 
+            double anchura = 45.123456789; // ocupa 8 bytes y es mas usado 
+            decimal moto = 9.99m; // usa mas bytes y es mas preciso y se una en 
+
+            Console.WriteLine("float");
+            float Numerosupergrande = float.MaxValue;
+            Console.WriteLine(Numerosupergrande);
+
+            Console.WriteLine("double");
+            double NumeroSupergrande = double.MaxValue;
+            Console.WriteLine(NumeroSupergrande);
+
+            Console.WriteLine("decimal");
+            decimal NumeroSuperGrande = decimal.MaxValue;
+            Console.WriteLine(NumeroSuperGrande);
         }
     }
 }
