cb579a11a6b967cec4c328f70b779c7d9309417a
laura0barrera <124647475+laura0barrera@users.noreply.github.com>
2023-02-10 18:13:19 -0500

    lo visto en clase
    todos los ejercicios presentados en clase

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Write("hola");
+            Console.WriteLine("holaaaa");
+            int edad = 18;
+            Console.WriteLine(edad);
+            string mensaje = "dx";
+            bool bandera = false;
+            var numero = 0;
+            int num1;
+            Console.WriteLine("escriba su nombre");
+            string nom = Console.ReadLine();
+            Console.WriteLine("el nombre del susurio es " + nom);
+            Console.ReadKey();
         }
     }
 }
