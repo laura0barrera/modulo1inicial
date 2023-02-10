94839948d8600c7e73467cb5ba0a80cdd68f4a60
laura0barrera <124647475+laura0barrera@users.noreply.github.com>
2023-02-10 18:06:57 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modulo1inicial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
